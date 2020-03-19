e20416aa64b28a58348260ac1ad7460d27c49f39
tanyushanel <tanyushanel@users.noreply.github.com>
2020-03-19 17:14:25 +0300

    feat: changed package.json & solution added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    return array && array.length ? Math.min(...array) : 0;
+    // return arr && arr.length ? Math.min.apply(Math, array) : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    return array && array.length ? Math.max(...array) : 0;
+    // return array && array.length ? Math.max.apply(Math, array) : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    return array && array.length ? array.reduce((a, b) => a + b, 0) / array.length : 0;
 }
